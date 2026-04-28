f1b9e66876058c47bd93e173ceafd43c83c441ff
NeonSmart <neon.cat.studio.840202@gmail.com>
2026-04-28 23:12:47 +0800

    F1 support CONFIG_STM32_HSE

--- src/W-Link/Driver/SysCtrl/Device/SysCtrl_STM32F1.c
+++ src/W-Link/Driver/SysCtrl/Device/SysCtrl_STM32F1.c
@@ -6,76 +6,74 @@
 
 #ifdef STM32F1
 
+#ifndef CONFIG_STM32_USE_HSE
+#define CONFIG_STM32_USE_HSE 0
+#endif
+
 void SysCtrl_Init()
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-#if defined(STM32F100xB) || defined(STM32F100xE) || defined(STM32F100x4) || defined(STM32F100x6)
+#if CONFIG_STM32_USE_HSE
 
-    /* STM32F100: max 24 MHz */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    /* ===== HSE: 72MHz ===== */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI;
     RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+
 #if defined(RCC_HSE_PREDIV_DIV1)
     RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
 #endif
-    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+
     RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL3;   /* 8MHz x 3 = 24MHz */
+    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL9;   // 8MHz → 72MHz
 
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
-        while(1); // error
+        while (1);
     }
 
-    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK  |
-                                       RCC_CLOCKTYPE_SYSCLK|
-                                       RCC_CLOCKTYPE_PCLK1 |
-                                       RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;   /* 24MHz */
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;     /* 24MHz */
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;     /* 24MHz */
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  // 36MHz
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  // 72MHz
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
     {
-        while(1); // error
+        while (1);
     }
 
 #else
 
-    /* F101/F103 常見 72MHz */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-#if defined(RCC_HSE_PREDIV_DIV1)
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-#endif
-    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
-    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL9;   /* 8MHz x 9 = 72MHz */
+    /* ===== HSI: 64MHz ===== */
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+
+    RCC_OscInitStruct.PLL.PLLState  = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+    RCC_OscInitStruct.PLL.PLLMUL    = RCC_PLL_MUL16;   // 4MHz → 64MHz
 
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
-        while(1); // error
+        while (1);
     }
 
-    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK  |
-                                       RCC_CLOCKTYPE_SYSCLK|
-                                       RCC_CLOCKTYPE_PCLK1 |
-                                       RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;   /* 72MHz */
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;     /* 36MHz */
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;     /* 72MHz */
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  // 32MHz
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  // 64MHz
 
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
     {
-        while(1); // error
+        while (1);
     }
 
 #endif
+
+    SystemCoreClockUpdate();
 }
 
 #endif //STM32F1
